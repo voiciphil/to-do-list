9e29ec64411b75edc4fc2cea8839ec394c1a6efe
Seong-phil Park <voiciphil@gmail.com>
2020-02-28 17:10:47 +0900

    chore: css 파일이 연관된 파일에만 적용되도록 수정

--- src/components/InputBar/InputBar.js
+++ src/components/InputBar/InputBar.js
@@ -3,10 +3,12 @@ import './InputBar.css';
 
 const InputBar = () => {
   return (
-    <div className="input-group">
-      <input type="text" className="form-control" />
-      <div className="input-group-append">
-        <button type="submit" className="btn btn-success">추가</button>
+    <div className="InputBar">
+      <div className="input-group">
+        <input type="text" className="form-control" />
+        <div className="input-group-append">
+          <button type="submit" className="btn btn-success">추가</button>
+        </div>
       </div>
     </div>
   );
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,18 +3,20 @@ import './List.css';
 
 const List = () => {
   return (
-    <div className="card">
-      <div className="row no-gutters">
-        <div className="col-md-1 card-body form-check">
-          <input type="checkbox" className="form-check-input checkbox" />
-        </div>
-        <div className="col-md-10 card-body">
-          asdf
-        </div>
-        <div className="col-md-1 delete">
-          <button type="button" className="close" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
+    <div className="List">
+      <div className="card">
+        <div className="row no-gutters">
+          <div className="col-md-1 card-body form-check">
+            <input type="checkbox" className="form-check-input checkbox" />
+          </div>
+          <div className="col-md-10 card-body">
+            asdf
+          </div>
+          <div className="col-md-1 delete">
+            <button type="button" className="close" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
         </div>
       </div>
     </div>
